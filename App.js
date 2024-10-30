caee1abfd25bdfc784a350e81c86d3b8b126d97f
FlorFonseca <florencia.fonseca@correo.ucu.edu.uy>
2024-10-30 13:20:26 -0300

    TA2 parte 2, terminado

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import { StatusBar } from "expo-status-bar";
-import { ScrollView, StyleSheet, Text, View, Image } from "react-native";
+import { ScrollView, StyleSheet, Text, View, Image, Button } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
 const images = [
   {
@@ -35,24 +37,62 @@ const images = [
   },
 ];
 
-export default function App() {
+//Este es un componente para manejar la galería dinámica
+const DynamicGallery = () =>{
+  const [actualImageIndex, setActualImageIndex] = useState(0);//acá se va guardando el indice de la imágen que se va mostrando
+  
+  const changeImage = () =>{//va recorriendo por los indices. Con % vuelve al inicio
+    setActualImageIndex((prevIndex) => (prevIndex +1) % images.length);
+  };
 
   return (
     <View style={styles.container}>
-      <ScrollView >
-        {images.map((imagen) => (
-          <View key={imagen.id} style={styles.imageContainer}>
-            <Image
-              source={imagen.imageUrl}
-              style={styles.image}
-              resizeMode="cover"
-            />
-            <Text style={styles.description}>{imagen.description}</Text>
-          </View>
-        ))}
-      </ScrollView>
+      <Image
+        source={images[actualImageIndex].imageUrl}
+        style={styles.image}
+        resizeMode="cover"
+      />
+      <Text style={styles.description}>
+        {images[actualImageIndex].description}
+      </Text>
+      <Text style={styles.description}>
+        Imagen {actualImageIndex + 1} de {images.length}
+      </Text>
+      <Button title="Cambiar imágen" onPress={changeImage} />
       <StatusBar style="auto" />
     </View>
+  );
+}
+
+//Este componente se encarga de mostrar la galeria con la scroll view de la parte 1
+const GalleryScroll= ()=>{
+  return (
+  <ScrollView >
+    {images.map((imagen) => (
+      <View key={imagen.id} style={styles.imageContainer}>
+        <Image
+          source={imagen.imageUrl}
+          style={styles.image}
+          resizeMode="cover"
+        />
+        <Text style={styles.description}>{imagen.description}</Text>
+      </View>
+    ))}
+  </ScrollView>
+  );
+}
+
+const Tab = createBottomTabNavigator();
+
+export default function App() {
+
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Galería" component={GalleryScroll} />
+        <Tab.Screen name="Cambio Dinámico" component={DynamicGallery} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
@@ -65,15 +105,14 @@ const styles = StyleSheet.create({
   },
   imageContainer: {
     margin: 10,
-    padding:10,
     borderRadius: 10,
+    elevation:2,
     overflow: "hidden",
-    elevation: 2,
     backgroundColor: "#f0f0f0",
     alignItems: "center",
   },
   image: {
-    width: 200, 
+    width: 200,
     height: 200,
   },
   description: {
